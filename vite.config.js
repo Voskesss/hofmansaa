d7a5843728f96005ab7127f2fa3cfc61d7881923
Voskesss <josklijnhout@hotmail.com>
2025-10-02 19:29:37 +0200

    Fix: use command check for base path in dev mode

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
-  // Development (lokaal): altijd /
-  // Vercel (productie): altijd /
-  // GitHub Pages: /hofmansaa/
-  base: mode === 'development' || process.env.VERCEL ? '/' : '/hofmansaa/'
+  // Development (vercel dev, npm run start): altijd /
+  // Build voor Vercel productie: /
+  // Build voor GitHub Pages: /hofmansaa/
+  base: command === 'serve' || process.env.VERCEL === '1' ? '/' : '/hofmansaa/'
 }));
