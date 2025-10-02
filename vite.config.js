62c64e86470f424ec245a1aaf38b98e9afbd2670
Voskesss <josklijnhout@hotmail.com>
2025-10-02 15:01:13 +0200

    Revert base naar /hofmansaa/ - custom domain later

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Voor custom domain gebruik '/', voor GitHub Pages subfolder gebruik '/hofmansaa/'
   base: process.env.NODE_ENV === 'production' ? '/hofmansaa/' : '/'
 });
